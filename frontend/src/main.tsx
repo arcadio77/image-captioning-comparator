6b66a94b3b80be5ff47ef8da6c4077c86c18513c
Arkadiusz Sieradzki <66469918+arcadio77@users.noreply.github.com>
2025-07-15 11:26:22 +0200

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,9 @@
-// src/main.tsx
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import GalleryPage from './GalleryPage.tsx';
-import { ImageDataProvider } from './ImageDataContext.tsx'; // Your context provider
+import { ImageDataProvider } from './ImageDataContext.tsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
