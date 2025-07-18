138fff0a54ebcfb15d0ea6772e28418e669e8957
Mielecki <mieleckibartek@gmail.com>
2025-07-18 11:48:35 +0200

    Reduce number of open channels

--- backend/app/rabbitmq.py
+++ backend/app/rabbitmq.py
@@ -1,5 +1,6 @@
 import pika, json
 from config import RABBITMQ_URL
+from models import connections, channels
 
 def setup_connection():
     params = pika.URLParameters(RABBITMQ_URL)
@@ -9,14 +10,13 @@ def setup_connection():
     return connection, channel
 
 def publish_message(exchange, routing_key, message, properties=None):
-    connection, channel = setup_connection()
-    channel.exchange_declare(exchange=exchange, exchange_type='topic')
+    if not connections.get("default") or not channels.get("default"):
+        connections["default"], channels["default"] = setup_connection()
+    channels["default"].exchange_declare(exchange=exchange, exchange_type='topic')
 
-    channel.basic_publish(
+    channels["default"].basic_publish(
         exchange=exchange,
         routing_key=routing_key,
         body=json.dumps(message),
         properties=properties
     )
-
-    connection.close()
